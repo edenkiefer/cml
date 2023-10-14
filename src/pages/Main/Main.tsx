95f3cd2bd6692dc4b2f93cebd831375ed4c2d289
Eden Kiefer <edenkieferdev@gmail.com>
2023-10-14 11:02:53 -0300

    feat: add Input component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,36 @@
+import React from 'react'
 import Header from '../../components/Header'
+import Input from '../../shared/Input'
+import { phoneMask } from '../../utils/masks'
+import { Container } from './styles'
 
 function Main() {
-  return <Header title="Novo Pedido" />
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value
+    event.target.value = phoneMask(value)
+  }
+
+  return (
+    <>
+      <Header title="Novo Pedido" />
+      <Container>
+        <form>
+          <Input label="Cliente" type="text" placeholder="Ex.: João Augusto" />
+          <Input
+            label="Telefone"
+            type="tel"
+            placeholder="Ex.: (99) 99999-9999"
+            onChange={handleInputChange}
+          />
+          <Input
+            label="Endereço"
+            type="text"
+            placeholder="Ex.: Rua das Rosas 41"
+          />
+        </form>
+      </Container>
+    </>
+  )
 }
 
 export default Main
